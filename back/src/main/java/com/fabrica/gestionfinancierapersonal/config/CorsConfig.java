505d44c8a377ed69a9de2a0c0fcec148b7b6ebb8
Ubuntu <ubuntu@ip-172-31-18-138.ec2.internal>
2026-04-18 10:35:44 +0000

    arranques automaticos y un solo ip

--- back/src/main/java/com/fabrica/gestionfinancierapersonal/config/CorsConfig.java
+++ back/src/main/java/com/fabrica/gestionfinancierapersonal/config/CorsConfig.java
@@ -1,23 +1,32 @@
 package com.fabrica.gestionfinancierapersonal.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://3.223.15.88:5173",
-                        "http://3.223.15.88:3000",
-                        "http://localhost:5173",
-                        "http://localhost:4173",
-                        "http://localhost:3000"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+    @Bean
+    public CorsFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(
+                "http://3.223.15.88"
+        ));
+
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
